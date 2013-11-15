8984b16fcf1c8460c848e860f9b219873bdcdeaf
Erik Bjäreholt <erik.bjareholt@gmail.com>
2013-11-15 10:07:32 +0100

    Solution to lab7

--- lab7/src/Main.java
+++ lab7/src/Main.java
@@ -1,14 +1,52 @@
 import se.lth.cs.ptdc.cardGames.*;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        Lay123.main(100000);
+        Lay123.main(1000000);
     }
 }
 
 class CardDeck {
-    
+    private Card[] cards;
+    private int current;
+    private static Random rand;
+
+    public CardDeck() {
+        cards = new Card[52];
+        rand = new Random();
+        int i = 0;
+        for (int suit=Card.SPADES; suit <= Card.CLUBS; suit++) {
+            for (int rank=1; rank<14; rank++) {
+                cards[i] = new Card(suit, rank);
+                i += 1;
+            }
+        }
+        current = 0;
+    }
+
+    public Card getCard() {
+        current++;
+        return cards[current-1];
+    }
+
+    public boolean moreCards() {
+        return ((current > 51) ? false : true);
+    }
+
+    public void shuffle() {
+        for (int i=0; i<52; i++) {
+            switchPlaces(i, rand.nextInt(52));
+        }
+    }
+
+    public void switchPlaces(int i1, int i2) {
+        Card temp = cards[i1];
+        cards[i1] = cards[i2];
+        cards[i2] = temp;
+    }
 
 }
 
@@ -35,13 +73,15 @@ class Lay123 {
         while (deck.moreCards()) {
             Card c = deck.getCard();
             if (n == c.getRank()) {
-                break;
+                return false;
+            } else {
+                //System.out.println("n: " + n + "   crank: " + c.getRank());
             }
             n++;
             if (n > 3) {
                 n = 1;
             }
         }
-        return (deck.moreCards() ? false : true);
+        return true;
     }
 }
